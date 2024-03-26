ebca826424fffc99e5f953f61c10109ccfddd82c
MyroslavaRogoza <miroslavarogozayyy@gmail.com>
2024-03-26 23:41:09 +0200

    Contact Form

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,25 +3,27 @@ import "./App.css";
 import ContactForm from "./ContactForm/ContactForm";
 import SearchBox from "./SearchBox/SearchBox";
 import ContactList from "./ContactList/ContactList";
-const contacts = [
-  { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-  { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-  { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-  { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-];
+import initialcContacts from "../initialContacts.json";
 
 function App() {
   const [filter, setFilter] = useState("");
+  const [contacts, setContacts] = useState(initialcContacts);
   const filteredContacts = contacts.filter((contact) => {
     return (
       contact.name.toLowerCase().includes(filter.toLowerCase()) ||
       contact.number.toLowerCase().includes(filter.toLowerCase())
     );
   });
+  function onAddContact(formContact) {
+    const finalFormContact = {
+      ...formContact,
+    };
+    setContacts((prevContact) => [...prevContact, finalFormContact]);
+  }
   return (
     <div>
       <h1>Phonebook</h1>
-      <ContactForm />
+      <ContactForm onAddContact={onAddContact} />
       <SearchBox filter={filter} onFilter={setFilter} />
       <ContactList contacts={filteredContacts} />
     </div>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import { ErrorMessage, Field, Form, Formik } from "formik";
+import { nanoid } from "nanoid";
+import { useId } from "react";
+import * as yup from "yup";
+import css from "./ContactForm.module.css";
 
-const ContactForm = () => {
+const ContactForm = ({ onAddContact }) => {
+  const handleSubmit = (values, actions) => {
+    console.log(values);
+    onAddContact(values);
+    actions.resetForm();
+  };
+  const FormInitialValues = {
+    id: nanoid(),
+    name: "",
+    number: "",
+  };
+  const nameId = useId();
+  const numberId = useId();
   return (
-    <div>ContactForm</div>
-  )
-}
+    <Formik initialValues={FormInitialValues} onSubmit={handleSubmit}>
+      <Form className={css.contactForm}>
+        <label htmlFor={nameId}>Name</label>
+        <Field type="text" name="name" id={nameId} />
+        <label htmlFor={numberId}>Number</label>
+        <Field type="number" name="number" id={numberId} />
+        <button type="submit">Add contact</button>
+      </Form>
+    </Formik>
+  );
+};
 
-export default ContactForm
+export default ContactForm;
