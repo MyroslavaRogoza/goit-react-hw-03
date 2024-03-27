43d7b002bf8236108d33d7c7887a24bb09aba630
MyroslavaRogoza <miroslavarogozayyy@gmail.com>
2024-03-27 13:02:22 +0200

    add icons

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,11 +1,21 @@
 import css from "./Contact.module.css";
+import { FaPeopleArrows } from "react-icons/fa6";
+import { FaPhoneSquare } from "react-icons/fa";
 
 const Contact = ({ contact, onDeleteContact }) => {
   return (
     <li className={css.contactItem}>
-      <h2>{contact.name}</h2>
-      <p>{contact.number}</p>
-      <button type="button" onClick={()=>onDeleteContact(contact.id)} className={css.delBtn}>
+      <h2>
+        <FaPeopleArrows /> {contact.name}
+      </h2>
+      <p>
+        <FaPhoneSquare /> {contact.number}
+      </p>
+      <button
+        type="button"
+        onClick={() => onDeleteContact(contact.id)}
+        className={css.delBtn}
+      >
         Delete
       </button>
     </li>
